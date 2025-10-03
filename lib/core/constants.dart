6cf72789854ebfb000216388735a9da9bea3d627
ANGELO UMALI <angeloumali165@gmail.com>
2025-10-03 12:47:28 +0800

    chores: Fix constants file

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
-const String OPENWEATHER_API_KEY = '9ea0e2012fd20267210b26383a231dbb';
+const String OPENWEATHER_API_KEY = 'Your_API_Key_Here';
 const String BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
